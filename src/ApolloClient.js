291e70d15e2da5206e05c1704538164e59370f0a
Kristine Alnes <kris.alnes@gmail.com>
2019-03-11 00:20:30 -0400

    Update semantic.json and add proxy for cors
    - Update semantic.json
    - Add proxy for cors

--- src/ApolloClient.js
+++ src/ApolloClient.js
@@ -1,7 +1,14 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: 'https://core-graphql.dev.waldo.photos/pizza'
+    uri: '/pizza',
+    request: async operation => {
+        operation.setContext({
+            fetchOptions: {
+                credentials: 'include'
+            }
+        });
+    }
 });
 
 export default client;
